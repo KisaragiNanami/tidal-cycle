e6c224869b5ae2412dba27fae6e60d92e685d36d
如月七海 <2959874980@qq.com>
2025-09-07 11:12:59 +0800

    124

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,8 @@ import remarkMath from "remark-math";
 
 import netlify from "@astrojs/netlify";
 
+import decapCmsOauth from "astro-decap-cms-oauth";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nanami7.top",
@@ -23,9 +25,13 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [expressiveCode(), mdx(), react(), sitemap(), compress(), terser({ compress: true, mangle: true }), icon()],
+  integrations: [expressiveCode(), mdx(), react(), sitemap(), compress(), terser({ compress: true, mangle: true }), icon(), decapCmsOauth()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
+});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
 });
