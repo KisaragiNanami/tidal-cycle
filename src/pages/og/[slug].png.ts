34676e342e96e2111f4167a2ad12438ad5a02459
如月七海 <2959874980@qq.com>
2026-01-25 18:40:34 +0800

    1.25

--- src/pages/og/[slug].png.ts
+++ src/pages/og/[slug].png.ts
@@ -19,10 +19,8 @@ export async function GET({
   props,
 }: APIContext<{ post: CollectionEntry<"blog"> }>) {
   const { post } = props;
-  const fontRegular = fs.readFileSync(
-    "public/fonts/MapleMono-NF-CN-Regular.woff2",
-  );
-  const fontBold = fs.readFileSync("public/fonts/MapleMono-NF-CN-Bold.woff2");
+  const fontRegular = fs.readFileSync("public/fonts/NotoSansSC-Regular.ttf");
+  const fontBold = fs.readFileSync("public/fonts/NotoSansSC-Bold.ttf");
   const iconBuffer = fs.readFileSync("public/favicon-dark.svg");
   const iconBase64 = `data:image/svg+xml;base64,${iconBuffer.toString("base64")}`;
 
