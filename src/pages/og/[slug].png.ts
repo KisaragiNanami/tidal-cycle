6af2db508ef3b8e10d4adc318fda35aeaec2c569
如月七海 <2959874980@qq.com>
2026-01-25 21:56:05 +0800

    fix the fonts problem

--- src/pages/og/[slug].png.ts
+++ src/pages/og/[slug].png.ts
@@ -19,8 +19,10 @@ export async function GET({
   props,
 }: APIContext<{ post: CollectionEntry<"blog"> }>) {
   const { post } = props;
-  const fontRegular = fs.readFileSync("public/fonts/NotoSansSC-Regular.ttf");
-  const fontBold = fs.readFileSync("public/fonts/NotoSansSC-Bold.ttf");
+  const fontRegular = fs.readFileSync(
+    "public/fonts/HarmonyOS_Sans_SC_Regular.woff2",
+  );
+  const fontBold = fs.readFileSync("public/fonts/HarmonyOS_Sans_SC_Bold.woff2");
   const iconBuffer = fs.readFileSync("public/favicon-dark.svg");
   const iconBase64 = `data:image/svg+xml;base64,${iconBuffer.toString("base64")}`;
 
